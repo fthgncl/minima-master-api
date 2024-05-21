190b2a07a42c1b7673104391a0fc1c1eddd42038
fthgncl <gencal.fatih61@gmail.com>
2024-05-21 16:10:57 +0300

    Evre hesaplaması düzeltildi

--- mathCalculations/calculatePhase.js
+++ mathCalculations/calculatePhase.js
@@ -3,7 +3,10 @@ let {Calc} = require('calc-js');
 function calculatePhase(time,startTime,period) { // EPOCH = ( time - startTime ) / period
     const timeDifferance = new Calc(time).minus(startTime).finish();
     const epoch = new Calc(timeDifferance).divide(period).finish();
-    const result = "0." + epoch.toString().split('.')[1];
+    let result = "0." + epoch.toString().split('.')[1];
+    if ( epoch < 0 ){
+        result = 1 - result;
+    }
     return parseFloat(result);
 }
 
